5d49c155aefb4809592ab424c2789ea4b698eb3e
ussr1674 <ussr_1674@proton.me>
2025-01-11 10:33:01 +0530

    cancunf: Rebrand for RisingOS
    Signed-off-by: ussr1674 <ussr_1674@proton.me>

--- lineage_cancunf.mk
+++ lineage_cancunf.mk
@@ -12,18 +12,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/cancunf/device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# RisingOS
+WITH_GMS := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+ifeq ($(strip $(WITH_GMS)),true)
+    TARGET_DEFAULT_PIXEL_LAUNCHER := true
+endif
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_cancunf
+PRODUCT_NAME := lineage_cancunf
 PRODUCT_DEVICE := cancunf
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g54 5G
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=cancunf \
     BuildDesc="cancunf_g_sys-user 14 U1TDS34.94-12-9-10-1 2d53a-f2829 release-keys" \
-    BuildFingerprint=motorola/cancunf_g_sys/cancunf:14/U1TDS34.94-12-9-10-1/2d53a-f2829:user/release-keys
+    BuildFingerprint=motorola/cancunf_g_sys/cancunf:14/U1TDS34.94-12-9-10-1/2d53a-f2829:user/release-keys \
+    RisingChipset="MT6855" \
+    RisingMaintainer="Soviet(ussr_1674)"
