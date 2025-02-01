3bef53e514a63686a20d827a9fe7192268b9dc3c
ussr1674 <ussr_1674@proton.me>
2025-02-01 10:44:03 +0530

    cancunf: Rebrand for Project Matrixx
    Signed-off-by: ussr1674 <ussr_1674@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/custom_cancunf.mk
+    $(LOCAL_DIR)/lineage_cancunf.mk
--- lineage_cancunf.mk
+++ lineage_cancunf.mk
@@ -12,14 +12,34 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/cancunf/device.mk)
 
 # Inherit some common PixelOS stuff.
+<<<<<<<< HEAD:custom_cancunf.mk
 $(call inherit-product, vendor/custom/config/common_full_phone.mk)
 TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_NAME := custom_cancunf
+========
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Project Matrixx
+MATRIXX_BUILD_TYPE := UnOfficial
+MATRIXX_CHIPSET := mt6855
+MATRIXX_BATTERY := 6000 mAh
+MATRIXX_DISPLAY := 1080×2400
+MATRIXX_MAINTAINER := Soviet(ussr_1674)
+# Matrixx GMS
+WITH_GMS := true
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := lineage_cancunf
+>>>>>>>> 264305d (cancunf: Rebrand for Project Matrixx):lineage_cancunf.mk
 PRODUCT_DEVICE := cancunf
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g54 5G
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
