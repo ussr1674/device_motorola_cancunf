f5ef8db328597c1b14a405fcfa25d3254a343d27
ussr1674 <ussr_1674@proton.me>
2025-02-20 16:54:27 +0530

    cancunf : Orionify
    Signed-off-by: ussr1674 <ussr_1674@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,4 +6,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cancunf.mk
+    $(LOCAL_DIR)/orion_cancunf.mk
--- orion_cancunf.mk
+++ orion_cancunf.mk
@@ -11,25 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/motorola/cancunf/device.mk)
 
-# Inherit some common PixelOS stuff.
+# Inherit some common OrionOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Project Matrixx
-MATRIXX_BUILD_TYPE := Unofficial
-MATRIXX_CHIPSET := mt6855
-MATRIXX_BATTERY := 6000 mAh
-MATRIXX_DISPLAY := 1080×2400
-MATRIXX_MAINTAINER := Soviet(ussr_1674)
-TARGET_INCLUDE_NEXUS := true
-
-# Matrixx GMS
-WITH_GMS := true
-BUILD_GOOGLE_CONTACTS := true
-BUILD_GOOGLE_DIALER := true
-BUILD_GOOGLE_MESSAGE := true
+#OrionOS
+ORION_MAINTAINER := Soviet
+ORION_MAINTAINER_LINK := https://t.me/ussr_1674
+ORION_BUILD_TYPE := Official
+ORION_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 1080
+# for devices that support udfps
+TARGET_HAS_UDFPS := false
+# Bootanimation for pixel device
+TARGET_PIXEL_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_cancunf
+PRODUCT_NAME := orion_cancunf
 PRODUCT_DEVICE := cancunf
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
