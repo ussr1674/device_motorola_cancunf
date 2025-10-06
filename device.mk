004dae393e6b5af0257186f5a8ba9eff136dfee8
ussr1674 <ussr_1674@proton.me>
2025-10-06 23:41:11 +0530

    cancunf: Update with device information

--- device.mk
+++ device.mk
@@ -458,3 +458,7 @@ PRODUCT_COPY_FILES += \
 # Inherit the proprietary files
 $(call inherit-product, vendor/motorola/cancunf/cancunf-vendor.mk)
 $(call inherit-product, vendor/motorola/cancunf-motcamera/cancunf-motcamera-vendor.mk)
+
+
+# Signed
+-include vendor/infinity-priv/keys/keys.mk
