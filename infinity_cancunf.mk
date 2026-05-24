69dd55d187465550e86c879641abaf04028ed624
Cyber Knight <cyberknight755@gmail.com>
2026-05-25 01:30:57 +0800

    cancunf: Update to `V1TDS35H.83-20-5-11`
    Change-Id: Ibf5a5381aa865b8d6a07b22f46260c15c74b1c6a
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- infinity_cancunf.mk
+++ infinity_cancunf.mk
@@ -37,5 +37,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=cancunf \
-    BuildDesc="cancunf_g_sys-user 15 V1TDS35M.83-20-5-3 b0a5b-963c9 release-keys" \
-    BuildFingerprint=motorola/cancunf_g_sys/cancunf:15/V1TDS35M.83-20-5-3/b0a5b-963c9:user/release-keys
+    BuildDesc="cancunf_g_sys-user 15 V1TDS35H.83-20-5-11 450c26-b11610 release-keys" \
+    BuildFingerprint=motorola/cancunf_g_sys/cancunf:15/V1TDS35H.83-20-5-11/450c26-b11610:user/release-keys
